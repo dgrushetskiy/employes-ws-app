512dab1af0c952188f476bf28fd7eaeab7ad2de6
dgrushetskiy <dgrushetskiy@gmail.com>
2024-03-18 02:11:35 +0300

    add framework project

---
+++ src/main/java/ru/gothmog/ws/employes/EmployesWsAppApplication.java
@@ -0,0 +1,13 @@
+package ru.gothmog.ws.employes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployesWsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployesWsAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/gothmog/ws/employes/EmployesWsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gothmog.ws.employes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployesWsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
